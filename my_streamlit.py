073b6b036760d590ff59f21537c843e3dfb77235
sujunwei <sujunwei@topgamesinc.com>
2021-11-05 10:34:00 +0800

    update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,17 +1,32 @@
 import os
 import sys
 import json
+import time
 import datetime
 import traceback
-import time
+from copy import deepcopy
 
 import requests
 import pandas as pd
 import numpy as np
 import streamlit as st
-from copy import deepcopy
+
 
 st.set_page_config(page_title="My Streamlit App",page_icon=":shark:",layout="wide")
+st.slider.radio('选择图表','Line','Bar')
+st.title(':sunny:')
+st.markdown('Streamlit is **_really_ cool**.')
+st.caption('This is a string that explains something above.')
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+st.text('This is some text.')
+st.latex(r'''a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =\sum_{k=0}^{n-1} ar^k =a \left(\frac{1-r^{n}}{1-r}\right)''')
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
 
 if "celsius" not in st.session_state:
     # set the initial default value of the slider widget
