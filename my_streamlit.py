0c0f7e149bbb068af3cdffb52163a9da49baa7f7
sujunwei <sujunwei@topgamesinc.com>
2021-11-09 09:50:20 +0800

    update

--- my_streamlit.py
+++ my_streamlit.py
@@ -35,9 +35,10 @@ def main():
         st.session_state.first_visit=False
     # 初始化配置
     if st.session_state.first_visit:
-        st.session_state.random_index=random.choice(range(len(charts_mapping)))
+        st.session_state.random_chart_index=random.choice(range(len(charts_mapping)))
         st.session_state.my_random=MyRandom(random.randint(1,1000000))
         st.session_state.city_mapping=get_city_mapping()
+        st.session_state.random_city_index=random.choice(range(len(st.session_state.city_mapping)))
         st.balloons()
 
     date_time=datetime.datetime.now() + datetime.timedelta(hours=8)
@@ -45,8 +46,8 @@ def main():
     t=st.sidebar.time_input('Time',date_time.time())
     t=f'{t}'.split('.')[0]
     st.sidebar.write(f'The current date time is {d} {t}')
-    chart=st.sidebar.selectbox('Select Chart You Like',charts_mapping.keys(),index=st.session_state.random_index)
-    city=st.sidebar.selectbox('Select City You Like',city_mapping.keys(),index=st.session_state.random_index)
+    chart=st.sidebar.selectbox('Select Chart You Like',charts_mapping.keys(),index=st.session_state.random_chart_index)
+    city=st.sidebar.selectbox('Select City You Like',st.session_state.city_mapping.keys(),index=st.session_state.random_city_index)
     st.markdown(f'### {chart} Chart')
     color = st.sidebar.color_picker('Pick A Color You Like', '#1535C9')
     st.sidebar.write('The current color is', color)
@@ -59,7 +60,7 @@ def main():
     # This will get the value of the slider widget
     st.sidebar.write(st.session_state.celsius)
 
-    weather=get_city_weather(city_mapping[city])
+    weather=get_city_weather(st.session_state.city_mapping[city])
     st.write(f"空气质量{weather['aqi']}\n体感温度{weather['realFeel']}")
 
     df=get_chart_data(chart,st.session_state.my_random)
