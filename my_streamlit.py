d208d42a76636dd24a596598aab2d5fcbdce3294
sujunwei <sujunwei@topgamesinc.com>
2021-11-09 17:09:06 +0800

    update

--- my_streamlit.py
+++ my_streamlit.py
@@ -100,8 +100,8 @@ def main():
             code=f.read()
         st.code(code,language="python")
 
-    # html='''<script>function($){document.getElementById("audio").play()}();</script>'''
-    html='''<script>alert("haha")</script>'''
+    html='''<script>document.getElementById("audio").play()</script>'''
+    # html='''<script>alert("haha")</script>'''
     components.html(html)
 
 class MyRandom:
