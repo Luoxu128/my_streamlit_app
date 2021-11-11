675adc2eb8e47eb9ad4ba58bf623c32b49792d6c
sujunwei <sujunwei@topgamesinc.com>
2021-11-11 13:21:46 +0800

    update

--- my_streamlit.py
+++ my_streamlit.py
@@ -93,6 +93,46 @@ def main():
         )
         components.html(c, width=1200, height=520)
 
+        c = (
+            Line(init_opts=opts.InitOpts(width="1600px", height="800px"))
+            .add_xaxis(xaxis_data=df_forecastDays.index.to_list())
+            .add_yaxis(
+                series_name="最高气温",
+                y_axis=df_forecastDays.Temperature.apply(lambda x:int(x.replace('°C','').split('~')[1])),
+                markpoint_opts=opts.MarkPointOpts(
+                    data=[
+                        opts.MarkPointItem(type_="max", name="最大值"),
+                        opts.MarkPointItem(type_="min", name="最小值"),
+                    ]
+                ),
+                markline_opts=opts.MarkLineOpts(
+                    data=[opts.MarkLineItem(type_="average", name="平均值")]
+                ),
+            )
+            .add_yaxis(
+                series_name="最低气温",
+                y_axis=df_forecastDays.Temperature.apply(lambda x:int(x.replace('°C','').split('~')[0])),
+                markpoint_opts=opts.MarkPointOpts(
+                    data=[opts.MarkPointItem(value=-2, name="周最低", x=1, y=-1.5)]
+                ),
+                markline_opts=opts.MarkLineOpts(
+                    data=[
+                        opts.MarkLineItem(type_="average", name="平均值"),
+                        opts.MarkLineItem(symbol="none", x="90%", y="max"),
+                        opts.MarkLineItem(symbol="circle", type_="max", name="最高点"),
+                    ]
+                ),
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="未来一周气温变化", subtitle="纯属虚构"),
+                tooltip_opts=opts.TooltipOpts(trigger="axis"),
+                toolbox_opts=opts.ToolboxOpts(is_show=True),
+                xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+            )
+            .render_embed()
+        )
+        components.html(c, width=1200, height=800)
+
 
     st.markdown(f'### {chart} Chart')
     df=get_chart_data(chart,st.session_state.my_random)
